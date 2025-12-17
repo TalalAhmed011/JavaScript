71333212dfc3c5f99ec56753591d4d69dd1bb00f
Talal Ahmed <talal445678@gmail.com>
2025-12-17 09:09:59 +0000

    date understood

---
+++ basics/math.js
@@ -0,0 +1,62 @@
+// const score=100;
+// console.log( score); 
+// const num=new Number(228.97);
+// console.log(num);
+// console.log(typeof(num));
+// console.log(num.toString());
+// console.log(num.toFixed(2));
+// console.log(num.valueOf());
+// console.log(num.toPrecision(3))
+
+// const num1=100000000;
+// console.log(num1.toLocaleString());
+// console.log("===================================");
+
+// const num2=2.5;
+// console.log(Math.round(num2));
+// console.log(Math.floor(num2));
+// console.log(Math.ceil(num2));
+// console.log(Math.trunc(num2));
+// console.log("===================================");
+
+// Math.random();
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()));
+// console.log(Math.ceil(Math.random()));
+// console.log((Math.random()*10)+1);
+
+
+// min=20;
+// max=30;
+// console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+// const date=new Date();
+
+// console.log(date);
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toISOString());
+// console.log(date.toLocaleString());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleTimeString());
+// console.log(date.getFullYear());
+// console.log(date.getMonth());
+// console.log(date.getDate());
+// console.log(date.getDay());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
+// console.log(date.getMilliseconds());
+// console.log(date.getTime());
+console.log("=============================");
+// console.log(Date.now());  //the dater from 1-jan-1970 to now in milliseconds
+
+let myCreatedDate = new Date("2025-12-17");
+console.log(myCreatedDate.toLocaleString());
+const mytimestamp=Date.now();
+console.log(mytimestamp);
+console.log(myCreatedDate.getTime());
+
+
+
+
